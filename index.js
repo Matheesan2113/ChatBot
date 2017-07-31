de6cefc8650b89124066ec6aef80148db446996e
Matheesan <mathee25@hotmail.com>
2017-07-31 11:42:19 -0400

    'initv15'

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-            if(text.toUpperCase()==="HI" or text.toUpperCase()==="HELLO"){
+            if(text.toUpperCase()==="HI" || text.toUpperCase()==="HELLO"){
                     sendText(sender,"Hello, how do you do?")
                     }
            // else if(text.indexOf('weather') >= 0){
