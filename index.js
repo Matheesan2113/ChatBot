7400a2fff34d51c6ecdbf2dd1a4ba88ddc4afefd
Matheesan <mathee25@hotmail.com>
2017-07-31 13:32:55 -0400

    'initv24'

--- index.js
+++ index.js
@@ -40,19 +40,19 @@ app.post('/webhook/', function(req, res) {
 		let sender = event.sender.id
 		if (event.message && event.message.text) {
 			let text = event.message.text
-            if(text==="hi"){
+            if(text.match(/"hi/i){
                     sendText(sender,"Hello, how do you do?")
                     }
            // else if(text.indexOf('weather') >= 0){
            //         sendText(sender,"The temperature in Toronto is "+temperature)
            //        }
-            else if(text.indexOf(/aaron/i) >= 0){
+            else if(text.match(/aaron/i){
                   sendText(sender,"yes. cause Aaron is a negro")
                   }
             else if(text.match(/Thuvaa/i)){
                   sendText(sender,"Thuvaa is trash at everything, but mostly ball and ow.")
                   }
-            else if(text==="/premium/i"){
+            else if(text.match(/premium/i){
                     sendText(sender,"I didn't do 5% because I might resell it")
                     }
             else{
